63d24e0acf520647b23414e17fbbad293d2375bb
Marc Reymann <mreymann@web.de>
2017-12-12 12:41:01 +0100

    added day 4 part 1

---
+++ 4_1.php
@@ -0,0 +1,10 @@
+#!/usr/bin/php
+<?php
+error_reporting( -1 );
+$valid = 0;
+$lines  = file( '4_1.in', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES );
+foreach ( $lines as $line_num => $line ) {
+	preg_match_all( '|\w+|', $line, $matches );
+	if ( count( array_unique( $matches[0], SORT_STRING ) ) == count( $matches[0] ) ) $valid++;
+}
+echo "valid: $valid\n";
