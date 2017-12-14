d6a83c58a018b852f2db4855a2646a6746008eab
Marc Reymann <mreymann@web.de>
2017-12-14 14:06:58 +0100

    added day 6 part 1

---
+++ 6_1.php
@@ -0,0 +1,26 @@
+#!/usr/bin/php
+<?php
+error_reporting( -1 );
+$file = trim( file_get_contents( '6_1.in' ) );
+preg_match_all( '|\d+|', $file, $matches );
+$banks = $matches[0];
+$states = array();
+$nrofbanks = count( $banks );
+$cycle = 1;
+while ( true ) {
+	$max = max( $banks );
+	$key = array_search( $max, $banks );
+	$banks[$key] = 0;
+	$j = $key;
+	for ( $i = 0; $i < $max; $i++ ) {
+		$j++;
+		if ( $j > $nrofbanks - 1 ) $j = 0;
+		$banks[$j] += 1;
+	}
+	if ( in_array( $banks, $states ) ) {
+		echo "known configuration seen after $cycle cycles\n";
+		exit;
+	}
+	$states[] = $banks;
+	$cycle++;
+}
